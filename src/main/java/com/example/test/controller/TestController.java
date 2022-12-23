1fa4af842c59b513b7ddc9c50077a1282b1c9b6c
Com-Sun <gnlslfflffl@gmail.com>
2022-12-23 17:44:07 +0900

    temp

--- src/main/java/com/example/test/controller/TestController.java
+++ src/main/java/com/example/test/controller/TestController.java
@@ -1,13 +1,20 @@
 package com.example.test.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
 
     @GetMapping("/index")
-    public String index() {
+    public String index(HttpServletRequest request, Model model){
+        String ip = request.getHeader("x-forwarded-for");
+        if(ip == null ){
+            ip = request.getRemoteAddr();
+        }
+        model.addAttribute("ip",ip);
         return "index";
     }
 
